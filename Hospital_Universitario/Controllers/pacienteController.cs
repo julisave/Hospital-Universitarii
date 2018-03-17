b7225b1d4b501149cb7f7bd91a41efa990dac0b4
Tatiana <leidy.cabrera@udea.edu.co>
2018-03-17 11:45:52 -0500

    Inicio Proyecto

---
+++ Hospital_Universitario/Controllers/pacienteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hospital_Universitario.Controllers
+{
+    public class PacienteController : Controller
+    {
+        //
+        // GET: /Paciente/Crear 
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+	}
+}
---
+++ Hospital_Universitario/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hospital_Universitario.Startup))]
+namespace Hospital_Universitario
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
